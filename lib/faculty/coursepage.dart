d636938ac76eb2624cdf445816e190d7dd317dba
dilip640 <2017csb1073@iitrpr.ac.in>
2018-12-19 00:10:17 +0530

    date picker added

--- lib/faculty/coursepage.dart
+++ lib/faculty/coursepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 
 import 'attendence_chart.dart';
 import 'floating_bar.dart';
@@ -10,28 +12,40 @@ class CourcePage extends StatefulWidget {
 }
 
 class _CoursePageState extends State<CourcePage> {
+  var formatter = new DateFormat('dd-MMM-yy');
   var _pages = ['26 OCT 18', '27 OCT 18', '28 OCT 18'];
   var _students = [
-    ['2017\n csb\n1073', true],
-    ['73', true],
-    ['74', false],
-    ['75', false],
-    ['76', true],
-    ['77', true],
-    ['78', false],
-    ['79', true],
-    ['80', true],
-    ['81', false],
-    ['82', true],
-    ['83', true],
-    ['84', true],
-    ['85', true]
+    ['2017csb1073', true],
+    ['2017csb1073', true],
+    ['2017csb1074', false],
+    ['2017csb1075', false],
+    ['2017csb1076', true],
+    ['2017csb1077', true],
+    ['2017csb1078', false],
+    ['2017csb1079', true],
+    ['2017csb1080', true],
+    ['2017csb1081', false],
+    ['2017csb1082', true],
+    ['2017csb1083', true],
+    ['2017csb1084', true],
+    ['2017csb1085', true]
   ];
-  String _curdate;
+  DateTime selectedDate = DateTime.now();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
 
   @override
   void initState() {
-    _curdate = _pages[0];
     super.initState();
   }
 
@@ -42,44 +56,44 @@ class _CoursePageState extends State<CourcePage> {
         floatingActionButton: FancyFab(), //----floating button
         body: Container(
             child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  
-                  children: [
-                    RaisedButton(
-                      padding: const EdgeInsets.all(8.0),
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      onPressed: () {},
-                      child: new Text("26 OCT 18"),
-                    ),
-                    RaisedButton(
-                      padding: const EdgeInsets.all(8.0),
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      onPressed: () {},
-                      child: new Text("See Images"),
-                    )
-                  ],
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RaisedButton(
+                  padding: const EdgeInsets.all(8.0),
+                  textColor: Colors.white,
+                  color: Colors.blue,
+                  onPressed: () {
+                    _selectDate(context);
+                  },
+                  child: new Text(formatter.format(selectedDate)),
                 ),
-                Expanded(
-                     child: _MakeListTile("26 OCT 18", _students))
+                RaisedButton(
+                  padding: const EdgeInsets.all(8.0),
+                  textColor: Colors.white,
+                  color: Colors.blue,
+                  onPressed: () {},
+                  child: new Text("See Images"),
+                )
               ],
-            )));
+            ),
+            Expanded(child: _MakeListTile(selectedDate, _students))
+          ],
+        )));
   }
 }
 
 class _MakeListTile extends StatefulWidget {
-  var _student;
-  String date;
+  final List _student;
+  final DateTime date;
   _MakeListTile(this.date, this._student);
   @override
   _MakeList createState() => _MakeList();
 }
 
 class _MakeList extends State<_MakeListTile> {
-  String date;
+  DateTime date;
   List students;
   @override
   void initState() {
@@ -96,19 +110,25 @@ class _MakeList extends State<_MakeListTile> {
       itemCount: students.length,
       itemBuilder: (BuildContext context, int index) {
         return Card(
-          child: Row(
-            children: <Widget>[
-              Text(students[index][0]),
-              new Switch(
-                value: students[index][1],
-                onChanged: (bool value) {
-                  setState(() {
-                    students[index][1] = !students[index][1];
-                  });
-                },
-              )
-            ],
-          ),
+          child: Padding(
+              padding: EdgeInsets.all(2.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                      margin: EdgeInsets.fromLTRB(20.0, 0.0, 120.0, 0.0),
+                      child: Text(students[index][0],
+                          style: TextStyle(
+                              fontSize: 17.0, fontWeight: FontWeight.bold))),
+                  Switch(
+                    value: students[index][1],
+                    onChanged: (bool value) {
+                      setState(() {
+                        students[index][1] = !students[index][1];
+                      });
+                    },
+                  )
+                ],
+              )),
         );
       },
     );
